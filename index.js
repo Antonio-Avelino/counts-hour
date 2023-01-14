5180520f56f15731779d4396041efc1478192d33
Antonio <antonio316890@gmail.com>
2023-01-14 00:02:10 +0000

    date

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-let {Synccreatehour,createhour}= require("./lib/hora")
+let {Synccreatehour,dates,createhourCall,createhourPromise}= require("./lib/hora")
+
+// console.log(Synccreatehour("1 6, 2021","22:45","1 6, 2021","05:30"))
+
+// console.log(createhourCall("1 6, 2021","22:45","1 6, 2021","05:30",(data)=>{
+
+//     console.log( data)
+// }))
+// createhourCall("1 6, 2021","22:45","1 6, 2021","05:30",(error,data)=>{
+
+//     console.log(data)
+    
+//     console.log(error)
+// })
 
 
+createhourPromise("1 6, 2021","22:45","1 6, 2021","05:30")
+.then((date)=>{
 
-// console.log(Synccreatehour("13:40","15:30")
-// )
-
-createhour("13:40","15:30",(data)=>{
-    console.log(data)
+    console.log(date)
 })
+    // console.log(dates("1 5, 2021","January 5, 2021"))
+
+
