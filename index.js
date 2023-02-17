9e78a03c4b67ededf5c0a46a03fe648dfc7df0f0
Antonio <antonio316890@gmail.com>
2023-02-17 22:05:36 +0000

    hour

--- index.js
+++ index.js
@@ -4,21 +4,3 @@ let {hourSync,date,hourCall,hourPromise} = require("./lib/hour")
 module.exports={hourSync,date,hourCall,hourPromise};
  
 
-// console.log(hourSync('2022-01-01', '22:00', '2022-01-03', '08:00'))
-// hourCall('2022-01-01', '13:30', '2022-01-02', '14:34',(data,erro)=>{
-
-//     console.log(data)
-// })
-
-
-
-// hourPromise('2022-01-00', '13:30', '2022-01-02', '14:34')
-// .then((date)=>{
-
-//     console.log(date)
-// })
-
-
-// console.log(date('2022-01-01', '2022-01-02'))
-
-
